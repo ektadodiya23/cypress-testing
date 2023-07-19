e714d42caebc1a8f7f649bb0ba1208a4d5e720d0
Ekta Dodiya <Ekta.Dodiya@MSBC-MAINFRAME.LCL>
2023-07-19 09:04:23 +0100

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import Home from './Component/filefolder/Home';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+
+function App() {
+  
+  return (
+    <div className="App">
+      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="*" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
+}
+
+export default App;
